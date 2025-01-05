5ab6dde6199a3d9f8be783e65231e36ca0b2f128
Justmore5mins <justmore5mins@gmail.com>
2025-01-06 07:54:51 +0800

    add

--- Annotating/transformer.py
+++
@@ -1,6 +0,0 @@
-from json import load
-from sys import argv
-
-targ_dir = argv[1]
-
-for item in 
--- src/Preprocess.py
+++ src/Preprocess.py
@@ -1,10 +1,10 @@
-from os import listdir,mkdir
+from os import listdir,mkdir,system
 from sys import argv
 from hashlib import md5
 from shutil import copyfile
-
 
 #hash the filenames
 for dir in argv[1:]:
     for file in listdir(dir):
         copyfile(f"{dir}/{file}", f"spilted/{md5(file.encode()).hexdigest()}.{file.split('.')[-1]}")
+        system(f"rm {dir}/{file}")
